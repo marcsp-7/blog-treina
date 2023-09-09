d0fa69fabdefa42247bedd3059daa4ed0dff06d2
marcsp-7 <kynho03@gmail.com>
2023-09-09 13:05:48 -0300

    Criação do Projeto

---
+++ src/main/java/com/blog/treina/BlogTreinaApplication.java
@@ -0,0 +1,13 @@
+package com.blog.treina;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogTreinaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogTreinaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blog/treina/BlogTreinaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blog.treina;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogTreinaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
